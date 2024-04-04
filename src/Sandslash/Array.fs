8ae910c33d8efefaf7c7517d724aa192326de129
midoliy <midoliy@outlook.jp>
2024-04-04 17:32:48 +0900

    wip

--- src/Sandslash/Array.fs
+++ src/Sandslash/Array.fs
@@ -3,12 +3,13 @@
 open System.Linq
 
 module Array =
-  let inline all ([<InlineIfLambda>]predicate: ^T -> bool) (array: ^T[]) = array.All(predicate)
-  let inline any ([<InlineIfLambda>]predicate: ^T -> bool) (array: ^T[]) = array.Any(predicate)
+  let inline all ([<InlineIfLambda>]predicate: ^T -> bool) (array: array<^T>) = array.All(predicate)
+  let inline any ([<InlineIfLambda>]predicate: ^T -> bool) (array: array<^T>) = array.Any(predicate)
   let inline cast (src: array<^T>) = 
     let acc = Array.zeroCreate<^U>(src.Length)
     for i in 0..src.Length-1 do
       acc.[i] <- (^U : (static member op_Implicit: ^T -> ^U) src.[i])
     acc
-  let inline contains (value: ^T) (array: ^T[]) = array.Contains(value)
-  let inline asSpan (array: ^T[]) = System.MemoryExtensions.AsSpan(array)
+  let inline contains (value: ^T) (array: array<^T>) = array.Contains(value)
+  let inline chunk (size: int) (array: array<^T>) = array.Chunk(size).ToArray()
+  let inline asSpan (array: array<^T>) = System.MemoryExtensions.AsSpan(array)
