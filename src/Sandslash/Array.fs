8ba7bc32447c012dc4f6f232e6e0044dbfea4d3c
midoliy <midoliy@outlook.jp>
2024-04-04 13:58:09 +0900

    wip

--- src/Sandslash/Array.fs
+++ src/Sandslash/Array.fs
@@ -1,6 +1,14 @@
 ﻿namespace Sandslash
 
-open System
 open System.Linq
 
 module Array =
+  let inline all ([<InlineIfLambda>]predicate: ^T -> bool) (array: ^T[]) = array.All(predicate)
+  let inline any ([<InlineIfLambda>]predicate: ^T -> bool) (array: ^T[]) = array.Any(predicate)
+  let inline cast (src: array<^T>) = 
+    let acc = Array.zeroCreate<^U>(src.Length)
+    for i in 0..src.Length-1 do
+      acc.[i] <- (^U : (static member op_Implicit: ^T -> ^U) src.[i])
+    acc
+  let inline contains (value: ^T) (array: ^T[]) = array.Contains(value)
+  let inline asSpan (array: ^T[]) = System.MemoryExtensions.AsSpan(array)
