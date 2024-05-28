d2d3ef2f48d77e551d39e52571a4240acb576d79
midoliy <midoliy@outlook.jp>
2024-05-28 13:32:18 +0900

    wip

--- src/Sandslash.Performance/ArrayBenchmark.fs
+++ src/Sandslash.Performance/ArrayBenchmark.fs
@@ -8,6 +8,9 @@ type ArrayBenchmark() =
   let array = [| for _ in 1..100_000 do fake.Random.Int() |]
   let value = fake.Random.Int()
 
+  (* ******************
+    forall
+   ****************** *)
   [<Benchmark>]
   member __.Sandslash_Array_forall () =
    array |> Sandslash.Array.forall (fun x -> x % 2 = 0)
@@ -16,6 +19,9 @@ type ArrayBenchmark() =
   member __.FSharp_Collections_Array_forall ()=
    array |> FSharp.Collections.Array.forall (fun x -> x % 2 = 0)
 
+  (* ******************
+    exists
+   ****************** *)
   [<Benchmark>]
   member __.Sandslash_Array_exists ()=
     array |> Sandslash.Array.exists (fun x -> x % 2 = 0)
@@ -24,6 +30,9 @@ type ArrayBenchmark() =
   member __.FSharp_Collections_Array_exists ()=
     array |> FSharp.Collections.Array.exists (fun x -> x % 2 = 0)
 
+  (* ******************
+    contains
+   ****************** *)
   [<Benchmark>]
   member __.Sandslash_Array_contains ()=
    array |> Sandslash.Array.contains value
@@ -32,6 +41,10 @@ type ArrayBenchmark() =
   member __.FSharp_Collections_Array_contains ()=
    array |> FSharp.Collections.Array.contains value
 
+  
+  (* ******************
+    countBy
+   ****************** *)
   [<Benchmark>]
   member __.Sandslash_Array_countBy ()=
    array |> Sandslash.Array.countBy (fun x -> x % 2 = 0)
